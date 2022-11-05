58040b880aee7215eb6819f85a831824d9a7c469
AHTI6IOTIK <phsts@yandex.ru>
2022-11-05 22:52:56 +0300

    Выполнил задание

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main main = new Main();
+        List<Integer> transformedList = main.transformAndSplitList(
+            List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+        );
+        System.out.println("transformedList = " + transformedList);
+    }
+
+    private List<Integer> transformAndSplitList(List<Integer> source) {
+        return source.stream()
+            .map(el -> el % 2 == 0 ? ++el : el)
+            .skip(2)
+            .limit(5)
+            .collect(Collectors.toList());
     }
 }
